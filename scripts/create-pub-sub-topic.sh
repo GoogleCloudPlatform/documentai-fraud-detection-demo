e931e9ea288a717aa652b10f872fa4618ea97feb
Sugandha <sugandhachoudhary@gmail.com>
2021-04-16 13:42:30 -0400

    Create create-pub-sub-topic.sh

---
+++ scripts/create-pub-sub-topic.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+source "${DIR}/.env.local"
+
+gcloud pubsub topics \
+  create ${GEO_CODE_REQUEST_PUBSUB_TOPIC}
+  
+gcloud pubsub topics \
+  create ${KG_REQUEST_PUBSUB_TOPIC}
+  
